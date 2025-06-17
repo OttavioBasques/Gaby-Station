fd48048a3058ee70283720f981d4dc2ca31ecd91
Panela <107573283+AgentePanela@users.noreply.github.com>
2025-06-17 14:13:15 -0300

    Atualiza packed e cluster (#180)
    * atualizações insanas
    
    * chore: Automatically update REUSE headers
    
    ---------
    
    Co-authored-by: GabyChangelog <agentepanela2@gmail.com>

--- Content.Shared/_Gabystation/CCVar/CVars.Gaby.cs
+++ Content.Shared/_Gabystation/CCVar/CVars.Gaby.cs
@@ -1,5 +1,7 @@
+// SPDX-FileCopyrightText: 2025 AgentePanela <agentepanela@gmail.com>
 // SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Panela <107573283+AgentePanela@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 joshepvodka <86210200+joshepvodka@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 joshepvodka <guilherme.ornel@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
@@ -21,5 +23,5 @@ public sealed partial class GabyCVars
     /// Enables alternate job titles for players.
     /// </summary>
     public static readonly CVarDef<bool> ICAlternateJobTitlesEnable =
-        CVarDef.Create("ic.alternate_job_titles_enable", true, CVar.SERVER | CVar.REPLICATED);
+        CVarDef.Create("ic.alternate_job_titles_enable", false, CVar.SERVER | CVar.REPLICATED); // desativado por agora
 }
