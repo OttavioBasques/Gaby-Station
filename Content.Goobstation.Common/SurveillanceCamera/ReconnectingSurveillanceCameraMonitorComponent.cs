ee028c37cdf6eb1946c36b45223d7d35c5feed74
GabyChangelog <agentepanela2@gmail.com>
2025-08-31 13:07:33 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Common/SurveillanceCamera/ReconnectingSurveillanceCameraMonitorComponent.cs
+++ Content.Goobstation.Common/SurveillanceCamera/ReconnectingSurveillanceCameraMonitorComponent.cs
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Hagvan <22118902+Hagvan@users.noreply.github.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
