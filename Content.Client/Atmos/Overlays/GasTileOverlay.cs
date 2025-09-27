0aca607a34eec796ffe1230959cbf31c2176e7bd
GabyChangelog <agentepanela2@gmail.com>
2025-09-27 17:53:29 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Atmos/Overlays/GasTileOverlay.cs
+++ Content.Client/Atmos/Overlays/GasTileOverlay.cs
@@ -21,6 +21,8 @@
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 GabyChangelog <agentepanela2@gmail.com>
 // SPDX-FileCopyrightText: 2025 Kyle Tyo <36606155+VerinSenpai@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Kyoth25f <kyoth25f@gmail.com>
+// SPDX-FileCopyrightText: 2025 Rouden <149893554+Roudenn@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 TemporalOroboros <TemporalOroboros@gmail.com>
 //
