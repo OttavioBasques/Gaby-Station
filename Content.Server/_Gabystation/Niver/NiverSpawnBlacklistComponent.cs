eea577322e6baefc175857db61070e7535068fd1
Panela <107573283+AgentePanela@users.noreply.github.com>
2025-11-23 17:56:35 -0300

    Aniversario do Gaby (#437)
    * commit primeira
    
    * termina
    
    * chore: Automatically update REUSE headers
    
    * Update Content.Server/_Gabystation/Niver/NiverSpawnSystem.cs
    
    * Update Content.Shared/_Gabystation/CCVar/CVars.Gaby.cs
    
    * Update Resources/Prototypes/_Gabystation/gamemodes.yml
    
    * mapa
    
    * fix: oi
    
    * Update Resources/Prototypes/_Gabystation/gamemodes.yml
    
    ---------
    
    Co-authored-by: GabyChangelog <agentepanela2@gmail.com>
    Co-authored-by: Kyoth25f <kyoth25f@gmail.com>

---
+++ Content.Server/_Gabystation/Niver/NiverSpawnBlacklistComponent.cs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Server._Gabystation.Niver;
+
+[RegisterComponent]
+public sealed partial class NiverSpawnBlacklistComponent : Component
+{
+}
