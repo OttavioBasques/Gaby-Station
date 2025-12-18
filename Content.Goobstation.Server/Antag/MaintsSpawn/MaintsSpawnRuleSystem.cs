93e686f0587c1cd66c2c614584175f047505b845
GabyChangelog <agentepanela2@gmail.com>
2025-12-18 02:06:59 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Server/Antag/MaintsSpawn/MaintsSpawnRuleSystem.cs
+++ Content.Goobstation.Server/Antag/MaintsSpawn/MaintsSpawnRuleSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Antag;
 using Content.Server.StationEvents.Components;
 using Content.Server.StationEvents.Events;
--- Content.Goobstation.Server/Disease/DiseaseSystem.Effects.cs
+++ Content.Goobstation.Server/Disease/DiseaseSystem.Effects.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Chat.Systems;
 using Content.Goobstation.Shared.Disease;
 using Robust.Shared.Map;
